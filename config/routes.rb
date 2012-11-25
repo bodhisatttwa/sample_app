efbe36c9c4ce7bc298dc38fe8bd5871908ecfbbb
Tomasz Kaluzny <tomasz.kaluzny@windowslive.com>
2012-11-25 19:41:04 +0100

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 SampleApp::Application.routes.draw do
   root to: "static_pages#home"
 
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
   resources :users
   match '/signup', to: 'users#new'
 
